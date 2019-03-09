8c5d8ed6e6fa8655c26bc4041b1d80279216e5f2
Helder Garcia <helder.garcia@gmail.com>
2019-03-09 00:11:21 -0300

    Handle of wallet nodes json files

--- src/WalletNodes.cpp
+++ src/WalletNodes.cpp
@@ -1,6 +1,12 @@
-// Copyright (c) 2019 Niobio developers
+// Copyright (c) 2019 Niobio Cash developers <helder.garcia@gmail.com>
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#include <QFile>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonValue>
+#include <QSettings>
 #include <QByteArray>
 #include <QNetworkReply>
 #include <QNetworkRequest>
@@ -43,6 +49,7 @@ void WalletNodes::OnListReadFinished()
     }
   }
   Settings::instance().setRpcNodesList(currentRpcNodesList);
+  saveSettings(currentRpcNodesList);
 	NetworkCleanup();
 }
 
@@ -50,4 +57,24 @@ void WalletNodes::NetworkCleanup()
 {
 	myNetReply->deleteLater();
 	myNetReply = nullptr;
+}
+
+void WalletNodes::saveSettings(const QStringList &_RpcNodesList) {
+  QJsonArray wn_array;
+  QString url_str("url");
+  QString fee_str("fee");
+  for(auto item : _RpcNodesList) {
+    QJsonObject item_data;
+    item_data.insert(url_str, QJsonValue(item));
+    item_data.insert(fee_str, QJsonValue(999));
+    wn_array.push_back(QJsonValue(item_data));
+  }
+  QJsonObject final_object;
+  final_object.insert(QString("remoteNodes"), QJsonValue(wn_array));
+  QFile walletNodesFile(Settings::instance().getWalletNodesFile());
+  if (walletNodesFile.open(QIODevice::WriteOnly | QIODevice::Truncate)) {
+    QJsonDocument cfg_doc(final_object);
+    walletNodesFile.write(cfg_doc.toJson());
+    walletNodesFile.close();
+  }
 }
